e2f657981a719184ce828778af8042d26442cd20
Michał Kalbarczyk <fazibear@gmail.com>
2015-09-04 11:42:28 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Airbrakex.Mixfile do
   def project do
     [
       app: :airbrakex,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.0",
       description: "Airbrake Elixir Notifier",
       package: package,

61927fb351c949e814697c0483dbadd31dc72495
Brandon Richey <brichey@teladoc.com>
2016-01-27 16:08:17 -0500

    Fix the exception parser test to be less brittle per the version of Elixir used

--- test/airbrakex/exception_parser_test.exs
+++ test/airbrakex/exception_parser_test.exs
@@ -2,31 +2,22 @@ defmodule Airbrakex.ExceptionParserTest do
   use ExUnit.Case
 
   test "should parse exception" do
-
     exception = try do
       IO.inspect("test",[],"")
     rescue
       e -> e
     end
 
-    result = %{
-      backtrace: [
-        %{file: "(Elixir.IO) lib/io.ex", function: "inspect(\"test\", [], \"\")", line: 209},
-        %{file: "(Elixir.Airbrakex.ExceptionParserTest) test/airbrakex/exception_parser_test.exs", function: "test should parse exception/1", line: 7},
-        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "exec_test/1", line: 293}, %{file: "(timer) timer.erl", function: "tc/1", line: 166},
-        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "-spawn_test/3-fun-1-/3", line: 242}
-      ],
-      message: "no function clause matching in IO.inspect/3",
-      type: FunctionClauseError
-    }
-
-    %{
+    expected = %{
       backtrace: [],
       message: "no function clause matching in IO.inspect/3",
       type: FunctionClauseError
     }
+    actual = Airbrakex.ExceptionParser.parse(exception)
 
-    assert Airbrakex.ExceptionParser.parse(exception) == result
+    assert [_head | _tail] = actual.backtrace
+    assert actual.message == expected.message
+    assert actual.type == expected.type
   end
 end
 

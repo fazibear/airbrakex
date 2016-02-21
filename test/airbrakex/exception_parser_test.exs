32fa6cc5a55deec0e0e0f9c7516f459f9fc5eae4
Michał Kalbarczyk <fazibear@gmail.com>
2016-02-21 18:01:13 +0100

    fix backtrace to match elixir 1.2

--- test/airbrakex/exception_parser_test.exs
+++ test/airbrakex/exception_parser_test.exs
@@ -11,10 +11,10 @@ defmodule Airbrakex.ExceptionParserTest do
 
     result = %{
       backtrace: [
-        %{file: "(Elixir.IO) lib/io.ex", function: "inspect(\"test\", [], \"\")", line: 201},
+        %{file: "(Elixir.IO) lib/io.ex", function: "inspect(\"test\", [], \"\")", line: 209},
         %{file: "(Elixir.Airbrakex.ExceptionParserTest) test/airbrakex/exception_parser_test.exs", function: "test should parse exception/1", line: 7},
-        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "exec_test/2", line: 253}, %{file: "(timer) timer.erl", function: "tc/1", line: 166},
-        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "-spawn_test/3-fun-1-/3", line: 201}
+        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "exec_test/1", line: 293}, %{file: "(timer) timer.erl", function: "tc/1", line: 166},
+        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "-spawn_test/3-fun-1-/3", line: 242}
       ],
       message: "no function clause matching in IO.inspect/3",
       type: FunctionClauseError
@@ -29,4 +29,3 @@ defmodule Airbrakex.ExceptionParserTest do
     assert Airbrakex.ExceptionParser.parse(exception) == result
   end
 end
-

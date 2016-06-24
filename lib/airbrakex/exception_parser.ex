92bb90323512f9b34db34cb1e21649ffb1735d72
Damir Gaynetdinov <damir@adjust.com>
2016-06-24 13:50:04 +0200

    Make it work with Elixir 1.3

--- lib/airbrakex/exception_parser.ex
+++ lib/airbrakex/exception_parser.ex
@@ -9,13 +9,13 @@ defmodule Airbrakex.ExceptionParser do
 
   defp stacktrace(stacktrace) do
     Enum.map stacktrace, fn
-      ({ module, function, args, [] }) ->
+      ({module, function, args, []}) ->
         %{
           file: "unknown",
           line: 0,
           function: "#{ module }.#{ function }#{ args(args) }"
         }
-      ({ module, function, args, [file: file, line: line_number] }) ->
+      ({module, function, args, [file: file, line: line_number]}) ->
         %{
           file: "(#{module}) #{List.to_string(file)}",
           line: line_number,
--- test/airbrakex/exception_parser_test.exs
+++ test/airbrakex/exception_parser_test.exs
@@ -1,31 +1,35 @@
 defmodule Airbrakex.ExceptionParserTest do
   use ExUnit.Case
 
-  test "should parse exception" do
-
+  test "parses exception" do
     exception = try do
-      IO.inspect("test",[],"")
+      IO.inspect("test", [], "")
     rescue
       e -> e
     end
 
-    result = %{
-      backtrace: [
-        %{file: "(Elixir.IO) lib/io.ex", function: "inspect(\"test\", [], \"\")", line: 209},
-        %{file: "(Elixir.Airbrakex.ExceptionParserTest) test/airbrakex/exception_parser_test.exs", function: "test should parse exception/1", line: 7},
-        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "exec_test/1", line: 293}, %{file: "(timer) timer.erl", function: "tc/1", line: 166},
-        %{file: "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex", function: "-spawn_test/3-fun-1-/3", line: 242}
-      ],
-      message: "no function clause matching in IO.inspect/3",
-      type: FunctionClauseError
-    }
+    parsed_exception = Airbrakex.ExceptionParser.parse(exception)
 
-    %{
-      backtrace: [],
-      message: "no function clause matching in IO.inspect/3",
-      type: FunctionClauseError
-    }
+    backtrace = parsed_exception[:backtrace]
+    message = parsed_exception[:message]
+    type = parsed_exception[:type]
 
-    assert Airbrakex.ExceptionParser.parse(exception) == result
+    assert type == FunctionClauseError
+    assert message == "no function clause matching in IO.inspect/3"
+
+    backtrace_files = Enum.map(backtrace, fn(entry) -> entry[:file] end)
+
+    assert Enum.member?(backtrace_files, "(Elixir.IO) lib/io.ex")
+    assert Enum.member?(backtrace_files, "(Elixir.Airbrakex.ExceptionParserTest) test/airbrakex/exception_parser_test.exs")
+    assert Enum.member?(backtrace_files, "(timer) timer.erl")
+    assert Enum.member?(backtrace_files, "(Elixir.ExUnit.Runner) lib/ex_unit/runner.ex")
+
+    backtrace_functions = Enum.map(backtrace, fn(entry) -> entry[:function] end)
+
+    assert Enum.member?(backtrace_functions, "inspect(\"test\", [], \"\")")
+    assert Enum.member?(backtrace_functions, "test parses exception/1")
+    assert Enum.member?(backtrace_functions, "exec_test/1")
+    assert Enum.member?(backtrace_functions, "tc/1")
+    assert Enum.member?(backtrace_functions, "-spawn_test/3-fun-1-/3")
   end
 end

4a01e2095b3fcdb25a6db34cdc5b59af79935180
Victor Rodrigues <victorc.rodrigues@gmail.com>
2017-07-25 12:37:56 +0200

    Fix Enum.filter_map/3 deprecation

--- lib/airbrakex/logger_parser.ex
+++ lib/airbrakex/logger_parser.ex
@@ -6,26 +6,41 @@ defmodule Airbrakex.LoggerParser do
 
   def parse(msg) do
     type = Regex.named_captures(@type_regex, msg)["type"]
+    lines = String.split(msg, "\n")
 
-    messages = Enum.filter_map String.split(msg, "\n"),
-          &(!Regex.match?(@stacktrace_regex, &1) &&
-            !Regex.match?(@exception_header_regex, &1)), fn(line) ->
+    message =
+      lines
+      |> Enum.filter(&message?/1)
+      |> Enum.map(fn line -> Regex.replace(@type_regex, line, "") end)
+      |> Enum.join("\n")
 
-              Regex.replace(@type_regex, line, "")
-    end
-
-    backtrace = Enum.filter_map String.split(msg, "\n"),
-        &(Regex.match?(@stacktrace_regex, &1)), fn(line) ->
-
-      hash = Regex.named_captures(@stacktrace_regex, line)
-      {line, _} = Integer.parse(hash["line"])
-      %{hash | "line" => line}
-    end
+    backtrace =
+      lines
+      |> Enum.filter(&stacktrace?/1)
+      |> Enum.map(&backtrace_line/1)
 
     %{
       type: type,
-      message: Enum.join(messages, "\n"),
+      message: message,
       backtrace: backtrace
     }
+  end
+
+  defp message?(line) do
+    !stacktrace?(line) && !exception_header?(line)
+  end
+
+  defp stacktrace?(line) do
+    Regex.match?(@stacktrace_regex, line)
+  end
+
+  defp exception_header?(line) do
+    Regex.match?(@exception_header_regex, line)
+  end
+
+  defp backtrace_line(line) do
+    hash = Regex.named_captures(@stacktrace_regex, line)
+    {line, _} = Integer.parse(hash["line"])
+    %{hash | "line" => line}
   end
 end

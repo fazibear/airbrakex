d8a24e21277b5a738f791e978df9ffeb98596f28
Guillaume Sempe <guillaume.sempe@gmail.com>
2018-07-09 14:57:17 +0100

    Fix stacktrace test

--- test/airbrakex/exception_parser_test.exs
+++ test/airbrakex/exception_parser_test.exs
@@ -36,6 +36,6 @@ defmodule Airbrakex.ExceptionParserTest do
     assert Enum.member?(backtrace_functions, "test parses exception/1")
     assert Enum.member?(backtrace_functions, "exec_test/1")
     assert Enum.member?(backtrace_functions, "tc/1")
-    assert Enum.member?(backtrace_functions, "-spawn_test/3-fun-1-/3")
+    assert Enum.member?(backtrace_functions, "-spawn_test/3-fun-1-/4")
   end
 end
